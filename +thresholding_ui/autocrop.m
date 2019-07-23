c58998c19c4b52fe9b25a1f51b332354931e0f67
Timothy A. Sipkens <tsipkens@gmail.com>
2019-07-23 16:39:42 -0700

    Added template for autocrop

---
+++ +thresholding_ui/autocrop.m
@@ -0,0 +1,14 @@
+
+% AUTOCROP Automatically crops an image based on binary information
+% Author:  Timothy Sipkens, Yeshun (Samuel Ma), 2019-07-23
+%=========================================================================%
+
+function [img_cropped,rect] = autocrop(img_orig,img_binary)
+
+
+
+img_cropped = [];
+rect = [];
+
+
+end
