98263aadab948a6cb4d0b430a6c5f0b752b22961
Timothy A. Sipkens <tsipkens@gmail.com>
2019-07-26 00:36:24 -0700

    Minor formatting update

--- +thresholding_ui/gyration.m
+++ +thresholding_ui/gyration.m
@@ -6,24 +6,24 @@
 %%%% "Observations of a Correlation between Primary Particle and Aggregate
 %%%% Size for Soot Particles", J. of Aerosol Sci. & Tech.
 
-function [ Radius_Gyration ] = Gyration(Final_Binary, pixsize)
+function [Rg] = gyration(Final_Binary, pixsize)
 %Gyration calculates radius of gyration by assuming every pixel as an area
 %of pixsize^2
 
 Total_area=nnz(Final_Binary)*pixsize^2;
 
-[xpos ypos]=find(Final_Binary);
+[xpos,ypos]=find(Final_Binary);
 n_pix=size(xpos,1);
 Centroid.x=sum(xpos)/n_pix;
 Centroid.y=sum(ypos)/n_pix;
 
 Ar2=zeros(n_pix,1);
 
-for k=1:n_pix;
+for k=1:n_pix
     Ar2(k,1)=(((xpos(k,1)-Centroid.x)*pixsize)^2+((ypos(k,1)-Centroid.y)*pixsize)^2)*pixsize^2;
 end
 
-Radius_Gyration=(sum(Ar2)/Total_area)^0.5;
+Rg = (sum(Ar2)/Total_area)^0.5;
 
 end
 
