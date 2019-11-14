405259ef985496a374d0bf28ca71190daa70c0a8
Timothy A. Sipkens <tsipkens@gmail.com>
2019-11-13 17:06:04 -0800

    Cleaned up get_img_ref.m

--- +tools/get_img_ref.m
+++ +tools/get_img_ref.m
@@ -10,8 +10,6 @@ img.num = 0; % 0: no image loaded; 1: at least one image loaded
 while img.num == 0
     dir_start = '..\Images'; % initial directory to look for images
     
-    message = sprintf('Please choose image(s) to be analyzed...');
-    
     [img.fname,img.dir] = uigetfile({'*.tif;*.jpg',...
         'TEM image (*.tif;*.jpg)'},'Select Images',dir_start,'MultiSelect',...
         'on');% User browses for images. Modify for other image formats
@@ -20,15 +18,7 @@ while img.num == 0
     if iscell(img.fname) % Handling when only one image is selected
         img.fname = img.fname';
     elseif img.fname==0
-        pixsize_choice=questdlg('No image was selected! Do you want to try again?', ...
-            'Error','Yes','No. Quit debugging','Yes');
-        
-        if strcmp(pixsize_choice,'No. Quit debugging')
-            uiwait(msgbox('No image was selected and user decided to stop the program'))
-            error('No image was selected and user decided to stop the program');
-        else
-            img.num= 0;
-        end
+        error('No image selected.');
     else
         img.fname = {img.fname};
     end
