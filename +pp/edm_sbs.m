df4ea2d5183acb065d804746ed8b74820dfc5e55
Timothy A. Sipkens <tsipkens@gmail.com>
2019-11-23 11:00:55 -0800

    Update edm_sbs.m

--- +pp/edm_sbs.m
+++ +pp/edm_sbs.m
@@ -13,12 +13,20 @@ function [Aggs] = edm_sbs(Aggs,bool_plot)
 
 
 %== Bin data after covering to nm =======%
-
+nb_classes = 25;
+d_max = 125;
+dpg = [0:d_max/(nb_classes-1):d_max];
 
 
 %== Fit inverse CDF to data ========%
 
 
 
+
+sigmoid = @(x) 1./(1+exp(((log(x(1))-log(dpg))./log(x(2))-1)./Omega));
+    % x(1) = dpg, x(2) = spg
+
+x0 = [30,1.5];
+x1 = lsqnonlin(@(x) sigmoid(x)-S, x0);
 
 end
