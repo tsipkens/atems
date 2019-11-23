099f58ecd4682e6e6ba602c321957f9ffcbe4634
Timothy A. Sipkens <tsipkens@gmail.com>
2019-11-23 02:39:36 -0800

    Create edm_sbs.m

---
+++ +pp/edm_sbs.m
@@ -0,0 +1,12 @@
+
+% EDM_SBS Performs Euclidean distance mapping-scale based analysis. 
+% Based on the work of Bescond et al. (2014)
+% Author: Timothy Sipkens, 2019-11-23
+%=========================================================================%
+
+function [Aggs] = edm_sbs(Aggs,bool_plot)
+
+
+
+
+end
