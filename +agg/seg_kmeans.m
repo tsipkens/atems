f0e295bf2fa8ce179d4fc3fc9a9dcec4911c4f84
Timothy A. Sipkens <tsipkens@gmail.com>
2020-09-09 21:11:23 -0700

    seg_kmeans now ignores <50 px
    Specifically, the bwareaopen function is now used to remove any aggregates below 50 pixels, where the method is generally less reliable.

--- +agg/seg_kmeans.m
+++ +agg/seg_kmeans.m
@@ -145,7 +145,7 @@ for ii=1:n
         % disk size must be less than se6 to maintain connectivity
     img_rb = imopen(i7, se7);
     
-    img_binary{ii} = bwareaopen(img_rb, 3); % remove particles below 3 pixels
+    img_binary{ii} = bwareaopen(img_rb, 50); % remove particles below 50 pixels
 %=========================================================================%
     
     
