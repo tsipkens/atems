74b4007ae9726111a71f4ef877bcf9d85c307347
Timothy A. Sipkens <tsipkens@gmail.com>
2020-09-10 09:46:22 -0700

    Bug fix for previous commit

--- +agg/analyze_binary.m
+++ +agg/analyze_binary.m
@@ -59,7 +59,7 @@ for ii=1:length(imgs_binary) % loop through provided images
     
     % If more than 55% of the image is aggregate, the method likely failed.
     % Skip this image and continue on.
-    if (nnz(imgs_binary) / numel(img_binary)) > 0.5; continue; end
+    if (nnz(img_binary) / numel(img_binary)) > 0.55; continue; end
 
     Aggs0 = struct([]); % re-initialize Aggs0 structure
     Aggs0(naggs).fname = '';
