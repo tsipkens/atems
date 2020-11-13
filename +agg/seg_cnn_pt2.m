61b145700131f4233028d4b8c4e8ef8ef267b5ec
Timothy A. Sipkens <tsipkens@gmail.com>
2020-11-13 15:33:31 -0500

    Added RB to seg_cnn_pt2
    That is, added an optional (only occurs if pixsize is given) rolling ball step to the function.

--- +agg/seg_cnn_pt2.m
+++ +agg/seg_cnn_pt2.m
@@ -4,7 +4,7 @@
 % seg_cnn_pt2.
 %=========================================================================%
 
-function [img_binary] = seg_cnn_pt2(fnames, fd)
+function [img_binary] = seg_cnn_pt2(fnames, fd, pixsize)
 
 %-- Get file names and parse inputs --------------------------------------%
 if isstruct(fnames) % convert input images to a cell array
@@ -24,13 +24,35 @@ if isempty(fd)
         error('Invalid CarbonBlackSegmentation directory.');
     end
 end
+
+if ~exist('pixsize', 'var'); pixsize = []; end
+if and(~isempty(pixsize), length(pixsize)==1)
+    pixsize = pixsize .* ones(size(fnames));  % extend pixel size if scalar
+end
 %-------------------------------------------------------------------------%
 
 img_binary = {};
 for ii=length(fnames):-1:1
     img_binary{ii} = imread([fd, '\results\', fnames{ii}]);
     img_binary{ii} = img_binary{ii}(:,:,1)~=0;
+
+        %== Rolling Ball Transformation ==============================%
+    if ~isempty(pixsize)  % if pixel size given, apply rolling ball transform
+        morph_param = 0.8/pixsize(ii); % parameter used to adjust morphological operations
+
+        ds = round(4 * morph_param);
+        se6 = strel('disk', max(ds, 1));
+            % disk size limited by size of holes in particle
+        i7 = imclose(img_binary{ii}, se6);
+
+        se7 = strel('disk', max(ds-1, 0));
+            % disk size must be less than se6 to maintain connectivity
+        img_rb = imopen(i7, se7);
+
+        img_binary{ii} = bwareaopen(img_rb, 50); % remove particles below 50 pixels
+    end    
 end
+
 
 end
 
