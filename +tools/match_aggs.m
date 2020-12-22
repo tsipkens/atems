4a53a9915fe8360bc10e7fe5dc8c30381e45adfc
tsipkens <tsipkens@gmail.com>
2020-12-22 16:03:58 -0500

    Update match_aggs: add px overlap
    Added a condition checking the degree of pixel overlap when matching aggregates. Also increased the cutoff for the distance between aggregate centers.
    + Substantial updates to commenting.

--- +tools/match_aggs.m
+++ +tools/match_aggs.m
@@ -5,45 +5,76 @@
 
 function [idx_a, idx_b] = match_aggs(Aggs_a, Aggs_b)
 
-% Initialize quantities.
+% Copy center of mass of each aggregate locally.
 A = [Aggs_a.center_mass]';
 B = [Aggs_b.center_mass]';
 
+% Initialize arrays to be filled in loop.
 A_cc = zeros(size(A));
 B_cc = zeros(size(B));
 cc_b = zeros(size(B,1), 1);
 
 
-% Loop through images in Aggs_a.
+% Loop through images in Aggs_a structure.
 for ii=1:max([Aggs_a.img_id])
     
-    idx_a = find([Aggs_a.img_id]==ii); % index in Aggs_a for this image
-    if isempty(idx_a); continue; end % if image isn't available
+    idx_a = find([Aggs_a.img_id]==ii);  % index in Aggs_a for this image
     
+    % If this image isn't available, the classifier failed. 
+    % Continue to next entry in loop.
+    if isempty(idx_a); continue; end
+    
+    % Repeat above procedure for Aggs_b.
     idx_b0 = find(strcmp({Aggs_b.fname}, Aggs_a(idx_a(1)).fname));
-    if isempty(idx_b0); continue; end % if image isn't available
-    idx_b0 = Aggs_b(idx_b0(1)).img_id; % initial image index in Aggs_b
-    idx_b = find([Aggs_b.img_id]==idx_b0); % index in Aggs_b for this image
+    if isempty(idx_b0); continue; end  % if image isn't available, i.e., classifier failed
     
-    for jj=idx_a % loop through aggregates in Aggs_a for this image
-        d = pdist2(Aggs_a(jj).center_mass', ...
-            B(idx_b, :)); % calculate the distance between 2 datasets 
-        d_min(jj) = min(d(:)); % find the minimum value of distance
+    idx_b0 = Aggs_b(idx_b0(1)).img_id;  % initial image index in Aggs_b
+    idx_b = find([Aggs_b.img_id]==idx_b0);  % index in Aggs_b for this image
+    
+    % Loop through aggregates in Aggs_a that were in this specific image.
+    for jj=idx_a
+        % Calculate the distance between the aggregates in the two datasets.
+        d = pdist2(A(jj, :), B(idx_b, :));
         
-        if d_min(jj)<150 % only consider if centers are close (within 150 px)
-            [~, row_b] = find(d == d_min(jj));
+        % Find the minimum value of distance. This corresponds to the
+        % aggregate with the center of mass closest to the current
+        % aggregate in Aggs_a.
+        d_min = min(d(:));
+        
+        % If centers are within 250 pixels, check that also closest in
+        % recipricol data (check also the closest aggregate to Aggs_b). 
+        if d_min<250
+            [~, row_b] = find(d == d_min);  % get index of match
             
             cc_b(idx_b(row_b)) = jj;
             B_cc(jj,:) = B(idx_b(row_b), :);
             A_cc(jj,:) = A(jj, :);
             
-            d = pdist2(A(idx_a, :), ...
-                B(idx_b(row_b), :)); % calculate the distance between 2 datasets 
-            d_min2 = min(d(:)); % find the minimum value of distance
+            d = pdist2(A(idx_a, :), B(idx_b(row_b), :));  % calculate the distance between two datasets 
+            d_min2 = min(d(:));  % find the minimum value of distance
             
-            % Check to make sure also the closest point in Aggs_a
-            if d_min2~=d_min(jj)
-                cc_b(idx_b(row_b)) = 0; % zero if not closest point in Aggs_a
+            % Check to make sure also the closest point in Aggs_a.
+            if d_min2~=d_min
+                cc_b(idx_b(row_b)) = 0;  % zero if not closest point in Aggs_a
+            
+            % Now, let's also check if the degree of overlap is logical.
+            % This prevents counting small fragments close to the center of
+            % the aggregate.
+            else
+                % Percent difference in number of pixels.
+                pd_np = 1 - abs((Aggs_b(idx_b(row_b)).num_pixels - ...
+                    Aggs_a(jj).num_pixels) / Aggs_a(jj).num_pixels);
+                
+                % Zero if not enough overlap. Note this step would
+                % automatically limit the magnitude of the discrepency 
+                % possible in aggregate diameter. Note the graduated
+                % threshold depending on size. For smaller aggregates, a
+                % slight shift in the aggregate center could cause larger
+                % discrepencies, and yet still be reasonable.
+                if and(pd_np < 0.3, Aggs_a(jj).num_pixels > 30000)
+                    cc_b(idx_b(row_b)) = 0;
+                end
+                
             end
         end
     end
