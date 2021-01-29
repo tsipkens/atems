2a7c2e5863a2975414c3c51d77612f0fcc355216
tsipkens <tsipkens@gmail.com>
2021-01-29 11:34:52 -0500

    Refactor agg.seg_slider header

--- +agg/seg_slider.m
+++ +agg/seg_slider.m
@@ -1,22 +1,28 @@
 
 % SEG_SLIDER Performs background correction and manual thresholding on a user-defined portion of the image.
-% Author:   Timothy Sipkens, 2019-10-11 (modified)
-%           Ramin Dastanpour, 2016-02 (based on)
-%           Developed at the University of British Columbia
-% 
-%-------------------------------------------------------------------------%
-% Inputs: 
-%   imgs          Either (1) an Imgs structure, (2) a cellular array of 
-%                 images, or (3) a single image.
-% 
-%   imgs_binary   Partially pre-classified binary image (allows for
-%                 modification to existing binary using this method).
-%                 (OPTIONAL)
-%   
-%   f_crop        Whether or not to run the image crop tool (to zoom in on
-%                 small parts of the image).
-%                 (OPTIONAL, default = 1)
-%=========================================================================%
+%  
+%  [IMGS_BINARY] = agg.seg_slider(IMGS) applies the slider method to the
+%  images speified in IMGS, an Imgs data structure. IMGS_BINARY is a binary
+%  mask resulting from the procedure.
+%  
+%  [IMGS_BINARY] = agg.seg_slider({IMGS}) applies the slider method to the
+%  images speified in IMGS, a cell of cropped images.
+%  
+%  [IMG_BINARY] = agg.seg_slider(IMG) applies the slider method to the
+%  single, copped image given by IMG.
+%  
+%  [IMG_BINARY] = agg.seg_slider(...,IMGS_BINARY) adds the options for a
+%  pre-classified binary image, which allows for modification of an
+%  existing binary mask. 
+%  
+%  [IMG_BINARY] = agg.seg_slider(...,IMGS_BINARY,F_CROP) adds a flag
+%  specifying whether of not to run the image crop tool to focus in on
+%  small part of the image. By default, F_CROP = 1 and does use the crop
+%  tools. Note, IMGS_BINARY can be empty.
+%  
+% AUTHOR: Timothy Sipkens, 2019-10-11 (modified)
+%  Ramin Dastanpour, 2016-02 (based on)
+%  Developed at the University of British Columbia
 
 function [imgs_binary] = seg_slider(imgs, imgs_binary, f_crop) 
 
