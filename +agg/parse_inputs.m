3f520ccea553fafe1baf5ac74f9583d277e2efd3
tsipkens <tsipkens@gmail.com>
2021-02-01 11:01:33 -0500

    Update parse_inputs pixsizes argument
    + No longer issues warning and fills pixsizes with 1, if empty input supplied. This allows for compatibility with method that do not require pixsizes.
    + Update to function header.
    + Added support for issuing error when NaN inputs supplied.

--- +agg/parse_inputs.m
+++ +agg/parse_inputs.m
@@ -4,7 +4,7 @@
 %  structure and outputs a common format. 
 %  
 %  [IMGS,PIXSIZES] = agg.parse_inputs(IMGS,[]) extracts all of the
-%  necessary information from IMGS, which must to an Imgs data structure.
+%  necessary information from IMGS, if it is a Imgs data structure.
 %  Returns the cropped images, IMGS, as a cell and the pixel sizes,
 %  PIXSIZES, as an appropriately lengthed vector. 
 %  
@@ -12,9 +12,18 @@
 %  appropriate format for the pixel sizes and image cell. Should result in
 %  minimal changes. 
 %  
+%  [IMGS,PIXSIZES] = agg.parse_inputs({IMGS},[]) ignores the pixsizer
+%  argument (under the assumption it is not required for the calling
+%  method (e.g., seg_slider). PIXSIZES will be returned as an empty vector. 
+%  
 %  [IMGS,PIXSIZES] = agg.parse_inputs(IMG,PIXSIZE) acts on a single image
 %  and pixel size, wrapping the IMG input in a cell and double checking the
-%  value of the pixel size. 
+%  value of the pixel size. As with preceeding option, PIXSIZE can be
+%  empty, in which case it will be ignored and an empty vector returned. . 
+%  
+%  [IMG,PIXSIZE,N] = agg.parse_inputs(...) also adds output for the number 
+%  of images in the set, N. This is a simple calculation of N =
+%  length(IMGS). 
 %  
 %  AUTHOR: Timothy Sipkens, 2021-01-29
 
@@ -43,12 +52,12 @@ if ~exist('pixsizes', 'var'); pixsizes = []; end % make sure pixsizes exists
 
 % See if there are any entries missing on non-sensical.
 % Return a warning about missing pixel sizes and replace with ones.
-if or(isempty(pixsizes), any(pixsizes==0))
-    warning(['Some pixel sizes were missing and assigned a value of one', ...
+if or(any(isnan(pixsizes)), any(pixsizes==0))
+    warning(['Some pixel sizes were missing and assigned a value of 1 nm/px. ', ...
         'This may cause errors in classification and should be checked.']);
     
     if isempty(pixsizes); pixsizes = ones(size(imgs));
-    else; pixsizes(pixsizes==0) = 1; end
+    else; pixsizes(pixsizes==0) = 1; pixsizes(isnan(pixsizes)) = 1; end
 end
 
 % Extend pxiel sizes if input is a scalar. 
