c15daf2ab24ab43d568eb77343dbf0a5093a1aa2
tsipkens <tsipkens@gmail.com>
2021-04-12 13:50:11 -0400

    Update main.py
    Reintroduce `if __name__ == '__main__':`.

--- carboseg/main.py
+++ carboseg/main.py
@@ -5,12 +5,13 @@ from PIL import Image
 
 from segmenter import Segmenter
 
+if __name__ == '__main__':
 
-image_paths = glob.glob("input/*.png")  # get all images in the input folder
+    image_paths = glob.glob("input/*.png")  # get all images in the input folder
 
-seg = Segmenter()  # create an instance of the classifier
-predictions = seg.run_images(image_paths)  # run the classifier to get predictions
+    seg = Segmenter()  # create an instance of the classifier
+    predictions = seg.run_images(image_paths)  # run the classifier to get predictions
 
-seg.save_predictions(predictions, image_paths)  # save the results to output folder
+    seg.save_predictions(predictions, image_paths)  # save the results to output folder
 
-Image.fromarray(predictions[0]).show()  # show a single prediction (the first image)
+    Image.fromarray(predictions[0]).show()  # show a single prediction (the first image)
