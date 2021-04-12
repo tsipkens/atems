1f99cf5b13bb87c61140d5c83507adfb30a11c55
tsipkens <tsipkens@gmail.com>
2021-04-12 13:35:35 -0400

    Update seg_ext, incl. bug fixes
    Function had fallen behind the rest of  codebase. Cleaned up inputs to match README.

--- +agg/seg_ext.m
+++ +agg/seg_ext.m
@@ -1,11 +1,11 @@
 
 % SEG_EXT  A simple function that saves cropped images for external segmentation.
-% This can then be used by external segmenters: 
-%   - The CarbonBlackSegmentation code before proceeding with seg_cnn_pt2.
-%   - Fiji in conjuction with trainable Weka segmentation.
+%  This can then be used by external segmenters: 
+%   + The carboseg code before proceeding with seg_cnn_pt2.
+%   + Fiji in conjuction with trainable Weka segmentation.
 %=========================================================================%
 
-function [fd] = seg_ext(imgs, fnames, fd)
+function [fnames] = seg_ext(imgs, fnames, fd, ext)
 
 %-- Get file names and parse inputs --------------------------------------%
 if isstruct(imgs) % convert input images to a cell array
@@ -17,19 +17,24 @@ end
 if ~iscell(imgs); imgs = {imgs}; end
 if ~iscell(fnames); fnames = {fnames}; end
 
+if ~exist('ext', 'var'); ext = []; end  % if file extension not specified
+
 if ~exist('fd', 'var'); fd = []; end
 if isempty(fd) % Browse to get path for CarbonBlackSegmentation code
     uiwait(msgbox( ...
-        ['Select upper directory of external code. ', ...
-        'Images will be placed in <strong>data</strong> folder.']));
-    [fd] = uigetdir('', ...
-        'Select directory');
+        ['Select folder to which to save images.']));
+    fd = uigetdir('', 'Select directory');
 end
 %-------------------------------------------------------------------------%
 
 
 % Write images to data folder
-tools.write_images(imgs, fnames, [fd, '\data'])
+disp('Saving images ...');
+fnames = tools.write_images(imgs, fnames, fd, ext);
+tools.textdone(2);  % print orange DONE
+
+
+if nargout==0; clear fnames; end  % clear output if not required
 
 end
 
